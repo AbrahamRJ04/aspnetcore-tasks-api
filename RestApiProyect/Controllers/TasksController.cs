1086b2dd69b6e4ce0d5a86991d8986cd9587926b
abrah <abrah@SHAGGY13>
2026-03-07 01:05:51 -0600

    Agregar archivos de proyecto.

---
+++ RestApiProyect/Controllers/TasksController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestApiProyect.Models;
+using System.Threading.Tasks;
+
+namespace RestApiProyect.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TasksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var tasks = await _context.Tareas.ToListAsync();
+            return Ok(tasks);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var task = await _context.Tareas.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Tareas task)
+        {
+            _context.Tareas.Add(task);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = task.Id }, task);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Tareas updateTask)
+        {
+            var task = await _context.Tareas.FindAsync(id);
+            if (task == null) return NotFound();
+
+            task.Titulo = updateTask.Titulo;
+            task.Descripcion = updateTask.Descripcion;
+            task.Estatus = updateTask.Estatus;
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var task = await _context.Tareas.FindAsync(id);
+            if (task == null) return NotFound();
+
+            _context.Tareas.Remove(task);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ RestApiProyect/Models/Tareas.cs
@@ -0,0 +1,11 @@
+﻿namespace RestApiProyect.Models
+{
+    public class Tareas
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string? Descripcion { get; set; }
+        public bool Estatus { get; set; }
+        public DateTime FechaVigencia { get; set; }
+    }
+}
